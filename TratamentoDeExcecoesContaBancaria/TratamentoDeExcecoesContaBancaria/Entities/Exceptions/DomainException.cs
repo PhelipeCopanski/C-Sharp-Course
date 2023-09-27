a79e247559e1f2931f28e6ee472d42d602c39e78
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-09-27 17:00:04 -0300

    Finaliza seção 11, resolve exercício de fixação

---
+++ TratamentoDeExcecoesContaBancaria/TratamentoDeExcecoesContaBancaria/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,7 @@
+﻿namespace TratamentoDeExcecoesContaBancaria.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+    }
+}
