8a9a77cb1943a5767fdd9fa89d38d1800ee670ae
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-10-02 10:05:49 -0300

    resolve exercício proposto da seção

---
+++ InterfacesPaymentProcess/InterfacesPaymentProcess/Program.cs
@@ -0,0 +1,33 @@
+﻿using InterfacesPaymentProcess.Entities;
+using InterfacesPaymentProcess.Services;
+using System.Globalization;
+
+namespace InterfacesPaymentProcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract Value: ");
+            double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int numberOfInstallments = int.Parse(Console.ReadLine());
+
+            Contract contract = new Contract(number, date, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(contract, numberOfInstallments);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in contract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
