dec1576a4acc6d325b72db90cc5e03f4346d7b3b
PhelipeCopanski <phelipe.copanski@gmail.com>
2023-10-04 15:15:00 -0300

    resolve exercíco da seção 15

---
+++ DictionaryEleicoes/DictionaryEleicoes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+
+namespace DictionaryEleicoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> candidates = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int votes = int.Parse(line[1]);
+                        
+                        if (candidates.ContainsKey(name))
+                        {
+                            candidates[name] += votes;
+                        }
+                        else
+                        {
+                            candidates[name] = votes;
+                        }
+                    }
+                    
+                    foreach (KeyValuePair<string, int> pair in candidates)
+                    {
+                        Console.WriteLine(pair.Key + ": " + pair.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
